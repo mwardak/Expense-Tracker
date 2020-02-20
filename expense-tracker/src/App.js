5df86e69670d3da44a3de4a094743a7e70ef9885
Maher Wardak <maher.wardak@gmail.com>
2020-02-19 20:55:48 -0500

    created a class

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,26 +1,32 @@
 
-import React from 'react';
+import React from "react";
 import Header from "./components/header";
 import Expense from "./components/expense";
 import ListExpenses from './components/listexpenses';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 // function to add expenses to table
 // Create HTML table
-function App() {
+// function to handle user input. function below will have access to state
 
-// function to handle user input
-  function handleSubmit(e){
-    console.log("this works");
+
+
+//Change functions below to a Class
+// Add set state to the class
+class App extends React.Component {
+  constructor(){
     
-
   }
+  
 
+
+ render(){
+  
   return (
     <div>
       <Header />
-      <Expense handleSubmit={handleSubmit} />
+      <Expense  />
       <ListExpenses />
     </div>
   );
